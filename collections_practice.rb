f4589ec94dc7b0339b335ab7d367501853922b01
Peter Chicarielli <peterchicarielli@gmail.com>
2017-03-09 03:27:39 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,21 +1,34 @@
+require 'pry'
+
 def begins_with_r(array)
-  array.all? { |word| word.upcase.start_with?("R")}
+  array.all? do |word|
+    word.start_with?("r")
+  end
 end
 
 def contain_a(array)
-  array.select {|word| word.include?("a")}
+  array.select do |word|
+    word.include?("a")
+  end
 end
 
 def first_wa(array)
-  array.find {|word| word.to_s.start_with?("wa")}
+  array.find do |word|
+    word.to_s.start_with?("wa")
+  end
 end
 
 def remove_non_strings(array)
-  array.delete_if {|word| word.class != String}
+  array.select do |element|
+    element.class == String
+  end
 end
 
 def count_elements(array)
-  array.uniq.each {|word| word[:count] = array.count(word)}
+  array.uniq.each do |word|
+    #binding.pry
+  word[:count] = array.count(word)
+  end
 end
 
 def merge_data(keys, data)
@@ -25,21 +38,22 @@ keys.each do |element|
   first_name = element[:first_name]
   keys = {:first_name => first_name}
   keys.merge!(data[first_name])
+  #binding.pry
   merged << keys
 end
 merged
 end
 
-def find_cool(array)
-  results = []
-  array.each do |i|
-    if i[:temperature] == "cool"
-      results << i
-    end
-  end
-  results
-end
 
+def find_cool(array)
+  result = []
+  array.find do |temp|
+      if temp[:temperature] == "cool"
+        result << temp
+  end
+end
+result
+end
 
 def organize_schools(array)
   list = {}
@@ -53,3 +67,19 @@ def organize_schools(array)
   end
   list
 end
+=begin
+
+def organize_schools(array)
+  list = {}
+  array.each do |city, school|
+    location = school[:location]
+    if list.has_key?(location)
+      list[location]<< city
+    else
+      list[location]= [city]
+    end
+  end
+  list
+end
+
+=end
