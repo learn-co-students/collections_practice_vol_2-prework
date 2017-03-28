d080c22a62fa6ec17037c81c9cb9283136ed4174
github username <github email address>
2017-03-28 00:47:50 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,17 +23,61 @@ def remove_non_strings(array)
 end
 
 def count_elements(array)
-    array.each_with_object(Hash.new(0)){|word, counts| counts[word]+=1}
+    #groups identical info together
+    group = array.group_by(&:itself)
+    #merges them together
+    group.map do |k, v|
+        k.merge(count: v.length)
+        end
 end
 
 def merge_data(keys, data)
-    keys.merge(data)
+    merged = []
+    keys.each_index do |i|
+        data[0].keys.each do |name|
+            if name == keys[i][:first_name]
+                merged<<keys[i].merge(data[0][name])
+                end
+            end
+        end
+    merged
+end
+            
+def find_cool(cool)
+    array = []
+    cool.each_index do |i|
+        if cool[i].has_value?("cool")
+           array<< cool[i]
+        end
+        end
+    array
 end
 
-def find_cool(array)
+#def organize_schools(schools)
+#    new_hash={}
+#    array = []
+#    schools.keys.each do |school_name|
+#        schools[school_name].each do |location, city|
+#            if schools[school_name][location] =
+#            new_hash[city]=schools.keys
+#            end
+#        end
+#end
+#    end
 
+def organize_schools(schools)
+    new_hash={}
+    schools.each do |school, info|
+        info.each do |location, city|
+            if new_hash.has_key?(city)
+                new_hash[city]<<school
+                else
+                new_hash[city]=[school]
+                end
+            end
+        end
+    new_hash
 end
 
-def organize_schools(array)
-
-end
+    
+    
