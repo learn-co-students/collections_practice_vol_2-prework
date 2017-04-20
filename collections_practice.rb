d50494c520ff2ddc6eeaf5a98c4197785790c9b8
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-04-20 21:16:18 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
-# your code goes here
+def begins_with_r(tools)
+  tools.all? {|tool| tool.start_with?("r")}
+end
+
+def contain_a(array)
+  array.select {|x| x.include?('a')}
+end
+
+def first_wa(array)
+  array.find {|x| x.to_s.start_with?('wa')}
+end
+
+def remove_non_strings(array)
+  array.delete_if {|x| x.class != String }
+end
+
+def count_elements(array)
+  array.map! {|x| x.merge(:count => 1)}
+  array.each_with_index do |x, i|
+    if x == array[i+1]
+      x[:count] += 1
+      array.delete(array[i + 1])
+    end
+  end
+end
+
+def merge_data(keys, data)
+  data[0].values.map.with_index {|x, i| keys[i].merge(x)}
+end
+
+def find_cool(cool)
+  cool.select {|x| x[:temperature] == "cool"}
+end
+
+def organize_schools(schools)
+  organized_schools = {}
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+    else
+      organized_schools[location] = []
+      organized_schools[location] << name
+    end
+  end
+  organized_schools
+end
