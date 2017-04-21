0a848970982adb35895474059f6993e4537e6e17
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-04-21 15:34:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,8 +34,8 @@ end
 
 def organize_schools(schools)
   organized_schools = {}
-  schools.each do |name, location_hash|
-    location = location_hash[:location]
+  schools.each do |name, city_hash|
+    location = city_hash[:location]
     if organized_schools[location]
       organized_schools[location] << name
     else
