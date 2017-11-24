910de3150ea68f810b2d3570b9953dd8985c24bc
TorreJohnson <torre.johnson@gmail.com>
2017-11-24 00:46:37 -0500

    completed 3/6 collections practice

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
-# your code goes here
+def begins_with_r(tools)
+  tools.each do |x|
+    if x[0].upcase != "R"
+      return false
+    end
+  end
+  return true
+end
+
+def contain_a(arr)
+  a_arr = []
+  arr.each do |x|
+    if x.include?("a")
+      a_arr << x
+    end
+  end
+  a_arr
+end
+
+def first_wa(elements)
+  elements.each do |x|
+    if x[0] == "w" && x[1] == "a"
+      return x
+    end
+  end
+end
+
+def remove_non_strings(arr)
+  arr.reject {|x| x.class != String}
+end
+
+def count_elements(arr)
+  arr.each_with_object(Hash.new(0)) {|arr, counts| counts[arr] += 1}
+end
+
+def merge_data(keys, data)
+  keys.flatten
+end
