d1d3f3193f5479fd19a189ccefeb710378c2920e
Christopher Diep <diep.christopher@gmail.com>
2017-12-15 15:35:31 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
-# your code goes here
+def begins_with_r(tools)
+  boolean_variable = nil
+  tools.each do | tool |
+    if tool[0] == "r" || tool[0] == "R"
+      boolean_variable = true
+    else
+      boolean_variable = false
+      return boolean_variable
+    end
+  end
+end
+
+def contain_a(array)
+  "a"
+end
