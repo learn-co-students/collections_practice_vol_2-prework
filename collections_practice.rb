d103cba6dd049ddb79b3227e045d90940e8b7904
Christopher Diep <diep.christopher@gmail.com>
2017-12-28 23:48:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,15 +1,46 @@
 def begins_with_r(tools)
   boolean_variable = nil
   tools.each do | tool |
-    if tool[0] == "r" || tool[0] == "R"
+    if tool[0] == "r"
       boolean_variable = true
     else
       boolean_variable = false
+      break if boolean_variable == false
     end
   end
-  return boolean_variable
+  boolean_variable
 end
 
 def contain_a(array)
-  "a"
+  array.select do | string |
+    string.include? "a"
+  end
+end
+
+def first_wa(array)
+  array.find do | string |
+    string[0..1] == "wa"
+  end
+end
+
+def remove_non_strings(array)
+  array.select do | element |
+    element.is_a? String
+  end
+end
+
+def count_elements(array)
+  array.each_with_object({}) {|object,hash| hash[object] +=1 }
+end
+
+def merge_data(keys,data)
+  keys + data
+end
+
+def find_cool(cool)
+  cool.select{|name,temp| temp[:temperature] == "cool"}
+end
+
+def organize_schools(schools)
+  schools
 end
