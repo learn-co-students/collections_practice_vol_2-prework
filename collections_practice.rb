8ea6e7131b51b415e1c91ea330a5aeb710528847
Mohammad Khan <sparrowangels@gmail.com>
2018-01-18 07:05:53 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,28 @@
+# #find_cool
+#   find all cool hashes (FAILED - 8)
+def find_cool(collection)
+  # puts "#{collection}"
+  return_hash_array = []
+  collection.each do |hash|
+    return_hash_array << hash if hash[:temperature] == "cool"
+  end #each
+  return_hash_array
+end
+
+
+# #organize_schools
+#   organizes the schools by location (FAILED - 9)
+def organize_schools(collection)
+  # puts "*******************#{collection}"
+  return_hash = {}
+  collection.each do |site, site_values|
+    return_hash[site_values[:location]] = [] if return_hash[site_values[:location]].nil?
+    return_hash[site_values[:location]] << site
+  end #each
+  return_hash
+end
+
+
 
 # #merge_data
 #   combines two nested data structures into one (FAILED - 7)
@@ -10,9 +35,45 @@
 # eness=>9, :height=>60, :last_name=>"dubs"}]
 
 def merge_data(collection1,collection2)
-  puts "**************#{collection1}"
-  puts "**************#{collection2}"
-  puts "*****************#{collection2[collection1.first[:first_name]]}"
+
+  return_hash_array = [] #collect combined hashes here
+  collection1.each do |c1_hash|
+    new_hash = {}
+    new_hash[:first_name]=c1_hash[:first_name]
+    collection2.each do |c2_hash| #find the matching hash in c2
+      # puts "*************#{c2_hash}"
+      c2_hash[new_hash[:first_name]].each do |k,v|
+        new_hash[k]=v
+      end #c2_hash.each
+    end #collection2
+    # puts "#{new_hash}"
+    return_hash_array << new_hash
+  end #collection1
+  return_hash_array
+#   collection1.each do |c1_hash|
+#     # puts "**********************#{c1_hash}" #{:first_name=>"blake"}
+#     # puts "**********************#{c1_hash.keys}" #[:first_name]
+#     c1_hash_keys = c1_hash.keys
+#     # puts "*********************#{c1_hash_keys[0]}" #first_name
+#     # puts "*********************#{c1_hash[c1_hash_keys[0]]}" #blake
+#     new_hash = {}
+#     new_hash[c1_hash_keys]=collection1[c1_hash_keys]
+#     puts "**************************#{new_hash}"
+#   end #loop through collection1 array
+
+  # puts "**************#{collection1}"
+  # puts "**************#{collection2}"
+  # puts "*****************#{collection2[collection1.first[:first_name]]}"
+  # return_hash_array = []
+  # collection1.each do |fname_key, fname|
+  #   return_hash = {}
+  #   return_hash[fname_key]=fname
+  #   collection2[fname].each do |k,v|
+  #     return_hash[k]=v
+  #   end #each collection2
+  #   return_hash_array << return_hash
+  # end #each collection1
+  # return_hash_array
 end
 
 
@@ -37,10 +98,6 @@ def count_elements(collection)
   # puts "********************#{newarr}"
   newarr
 end
-# #find_cool
-#   find all cool hashes (FAILED - 8)
-# #organize_schools
-#   organizes the schools by location (FAILED - 9)
 
 
 
