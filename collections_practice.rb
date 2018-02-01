097763cdde11a9b789457d28d6fc42accbfc890a
nkalkstein <nkalkstein@gmail.com>
2018-01-31 23:35:19 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,7 +34,18 @@ def count_elements(array)
   end
 end
 
-# def merge_data(keys, data)
-#
-#
-# end
+def merge_data(keys, data)
+   merged = []
+   keys.each {|i| data.first.map {|k,v| if i.values[0] == k then merged << i.merge(v) end}}
+   merged
+end
+
+def find_cool(cool)
+      cool.select {|i| i.any? {|k,v| v == "cool"}}
+ end
+
+ def organize_schools(schools)
+     locations_hash = {}
+     schools.collect {|k,v| locations_hash[v[:location]] = []}
+     locations_hash.each {|k,v| schools.each {|k1,v1| if k == v1[:location] then v << k1  end}}
+ end
