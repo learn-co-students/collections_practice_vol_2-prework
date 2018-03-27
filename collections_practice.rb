3d19b46c18d9715af6e8d0f8f342c66de8527f07
Ryan O'Toole <otoolerj@gmail.com>
2018-03-27 06:02:57 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r(tools_array)
+  tools_array.collect do |element|
+    if element.start_with?("r") == false
+      return false
+    end
+  end
+true
+end
+
+def contain_a(tools_array)
+  array = []
+  tools_array.collect do |element|
+    if element.include?("a")
+      array<<element
+    end
+  end
+array
+end
+
+def first_wa(tools_array)
+  tools_array.collect do |element|
+    if element.start_with?("wa") == true
+      return element
+    end
+  end
+end
