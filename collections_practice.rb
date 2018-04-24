4ed4a56517cdc3f6fcbc84d6ef99c3dbead040fa
jarretbryan <jarret.bryan@gmail.com>
2018-04-23 22:35:50 -0400

    solved first 3 methods

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
 # your code goes here
+require 'pry'
+
+ def begins_with_r(array)
+   array.each do |string|
+     if !string.start_with?("r" || "R")
+       return false
+     end
+   end
+   true
+ end
+
+ def contain_a(array)
+   array.select do |string|
+     string.include?("A".downcase)
+   end
+ end
+
+ def first_wa(array)
+   new_arr = []
+   array.each do |string|
+     if string.to_s.include?("wa".downcase)
+       new_arr.push(string.to_s)
+     end
+   end
+   new_arr[0]
+ end
