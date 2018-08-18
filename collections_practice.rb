e8408ddd4f8ebeb989c7488449fa1be74216bdb5
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-18 19:59:26 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
 require "pry"
+
+def begins_with_r
+  
+end 
+
+
+def contain_a
+  
+end
+
+
+def first_wa
+  
+end 
+
+
+def remove non_strings
+  
+end 
+
+
+def count_elements
+  
+end
+
+
+def merge_data
+  
+end 
+
+
+def find_cool
+  
+end 
+
+
+def organize_schools
+
+end  
+  
+  
