6707107fad2293fd305bebb5897d8ff7ab5dfdc5
Adam Johnson <adamwjo@gmail.com>
2018-08-23 14:58:19 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,94 @@
-# your code goes here
+
+def begins_with_r(array)
+  counter = 0
+  array.each do |item|
+    if item[0] != "r"
+      return false
+    else
+      if item[0] == "r"
+        counter += 1
+      end
+      if counter == array.length
+        return true
+      end
+    end
+  end
+end
+
+def contain_a(array)
+  new_array = []
+  array.each do |item|
+    if item.include?("a")
+      new_array.push(item)
+    end
+  end
+  new_array
+end
+
+def first_wa(array)
+  array.each do |item|
+    item = item.to_s
+    if item.start_with?("wa")
+      return item
+    end
+  end
+end
+
+def remove_non_strings(array)
+  new_array = []
+  array.each do |item|
+    if item.is_a?(String)
+      new_array.push(item)
+    end
+  end
+  new_array
+end
+
+def count_elements(array)
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |new_hash|
+      if new_hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
+end
+
+def merge_data(keys, values)
+  container = []
+  keys.each do |person_name|
+    name = person_name[:first_name]
+    values.each do |person_data|
+      if person_data[name]
+        merged_person = person_data[name]
+        merged_person[:first_name] = name
+        container << merged_person
+      end
+    end
+  end
+  container
+end
+
+def find_cool(array)
+  container = []
+  array.each do |element|
+    container << element if element[:temperature] == "cool"
+  end
+  container
+end
+
+def organize_schools(schools)
+  organized_schools = {}
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+    else
+      organized_schools[location] = []
+      organized_schools[location] << name
+    end
+  end
+  organized_schools
+end
