5c263c86a9ceb5299cdd125377d345a85749db53
Malcolm Leake <echelon1089@gmail.com>
2018-08-30 22:46:20 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,10 +2,15 @@ require 'pry'
 def begins_with_r(tools)
   test = true
   tools.each do |first_letter|
-   #binding.pry
     if first_letter[0] != "r"
        test = false
     end 
   end
 test
+end
+
+def contain_a(cheat)
+cheat.each do |contain_a|
+  
+#binding.pry
 end
