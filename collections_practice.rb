bbe27b3ffd03e312b0d49b9346a69d6b9aad3ddf
Malcolm Leake <echelon1089@gmail.com>
2018-08-30 21:57:35 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,9 @@
-# your code goes here
+require 'pry'
+def begins_with_r(tools)
+  tools.each do |first_letter|
+   binding.pry
+    if first_letter[0] == "r"
+      return true
+    end #binding.pry
+  end
+end
