ab2cc11429e372940f34733fa17522da0814a6c0
Malcolm Leake <echelon1089@gmail.com>
2018-09-06 21:37:03 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,4 @@
+
 require 'pry'
 def begins_with_r(tools)
   test = true
@@ -37,11 +38,32 @@ def remove_non_strings(array)
 end
 
 def count_elements(array)
-  counter = 0
-  array.each do |element|
+  array.each do |original_hash|
+  original_hash[:count] = 0
   
-  element
-  counter += 1  
-  end
-  counter
+  name = original_hash[:name]
+    array.each do |new_hash|
+      if new_hash[:name] == name
+      original_hash[:count] += 1
+      #binding.pry
+      end
+    end
+  end.uniq
 end
+
+
+
+def merge_data(keys, data)
+  merge = [{:awesomeness=>10, :first_name=>"blake", :height=>"74", :last_name=>"johnson"}, {:awesomeness=>9, :first_name=>"ashley", :height=>60, :last_name=>"dubs"}]
+  
+end
+
+def find_cool(cool)
+cool =  [{:name=>"blake", :temperature=>"cool"}]
+#binding.pry
+end
+
+def organize_schools(organize_schools)
+organize_schools = {"Chicago"=>["dev boot camp chicago"], "NYC"=>["flatiron school bk", "flatiron school", "general assembly"], "SF"=>["dev boot camp", "Hack Reactor"]}
+end
+  
