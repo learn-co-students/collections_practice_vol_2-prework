bf18914e9a666ff4c351c7c103eebdae7a3bbce2
Asaf Davidov <adavidov@conncoll.edu>
2018-09-12 19:40:36 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,4 +21,31 @@ def remove_non_strings(arr)
   arr.delete_if{|element| !element.instance_of?(String)}
 end
 def count_elements(arr)
+  
+end
+
+def merge_data(keys,data)
+
+end
+
+def find_cool(hashes)
+  cool_hashes = []
+  hashes.each do |hash|
+    if hash[:temperature] == "cool"
+      cool_hashes << hash
+    end
+  end
+  cool_hashes
+end
+
+def organize_schools(orig_schools)
+  organized_school = {}
+  orig_schools.each do |school|
+    if organized_school[school[1][:location]] == nil
+      organized_school[school[1][:location]] = [school[0]]
+    else
+      organized_school[school[1][:location]] << school[0]
+    end
+  end
+  organized_school
 end
