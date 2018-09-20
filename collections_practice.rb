94e3b97e4a1e814f36934d1f583da3edd5233b2b
Dario Spina <dario_spina01@live.it>
2018-09-20 23:08:30 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,3 +27,47 @@ def count_elements(array)
     end
     return answer
   end
+  
+  def merge_data(keys,data)
+    answer=[]
+    keys.each do |x|
+      x.each do |y,z|
+        data.each do |xx|
+          xx.each do |yy,zz|
+            if yy==z
+              answer.push(x.merge(zz))
+            end
+        end
+      end
+    end
+  end
+  return answer
+end
+
+def find_cool(input)
+  answer=[]
+  input.each do |x|
+if x.values_at(:temperature)[0]=="cool"
+      answer.push(x)
+end
+end
+answer
+end
+
+
+def organize_schools(input)
+  newhash=Hash.new
+  input.each do |x,y|
+    y.each do |w,z|
+      if newhash[z]==nil
+      newhash[z]=[x]
+    else
+
+      newhash[z].push(x)
+      
+    end
+  end
+  
+end
+return newhash
+end
