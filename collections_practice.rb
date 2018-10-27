694292f7ae7854a0fefd982fd59a45bfb35c0204
Connor Callahan <connorwcallahan@gmail.com>
2018-10-27 20:22:00 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,5 +28,23 @@ def first_wa(array)
   end
 end 
 
-def remove_non_strings
+def remove_non_strings(array)
+  string_array = []
+  array.each do |v|
+    if v.class == String
+      string_array << v
+    end 
+  end 
+  return string_array
 end 
+
+def count_elements(array)
+  counter = 0
+  array[:name].each |value|
+    if array.count(value) > 1 
+      counter = counter + 1 
+  end 
+  binding.pry
+  counter 
+end 
+
