c3100a789467f50ab09274d5dfae2bceed46445a
Connor Callahan <connorwcallahan@gmail.com>
2018-10-27 20:04:13 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
-# your code goes here
+require 'pry'
+
+
+def begins_with_r(array)
+  array.each do |v|
+    if v.start_with?("r") == false 
+      return false
+  end
+end
+  return true
+end
+
+def contain_a(array)
+  a_array = []
+  array.each do |v|
+    if v.include?("a")
+      a_array << v 
+      end 
+    end 
+  a_array     
+end
+
+def first_wa(array)
+  array.each do |v|
+    if v.to_s.start_with?("wa") 
+      return v
+    end 
+  end
+end 
+
+def remove_non_strings
+end 
