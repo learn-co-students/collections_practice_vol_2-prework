addefdabe0accd80c69662bd81e48ddf37cc5c36
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-17 12:34:02 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,3 +40,36 @@ def first_wa(array)
   end
 end
 
+
+# separated the string and hash instance of conditionals, putting them on one line with if String && Hash returned the string number and hash, my guess is this is because using both is messing with the truthyness of the conditional. Aso just wanted to try delete if and each for practice so using different enumerable methods
+def remove_non_strings(array)
+  array.delete_if do |i|
+    i.instance_of?(String) == false
+  end
+  array.each do |i|
+    if i.instance_of?(Hash) == true
+      array.delete(i)
+    end
+  end
+  return array
+end
+
+def
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
