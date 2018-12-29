20509a71292b2f6d6a77b203b24c68b7998bf1d0
Your Name <github email address>
2018-12-29 10:54:53 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,65 @@
-# your code goes here
+def begins_with_r(array)
+  flag = true
+  array.each do |element|
+    flag = false if element[0] != "r"
+  end
+  flag
+end
+
+def contain_a(array)
+  container = []
+  array.each do |element|
+    container << element if element.include?("a")
+  end
+  container
+end
+
+def first_wa(array)
+   array.detect { |word| word.to_s.start_with?("wa") }
+end
+
+def remove_non_strings(array)
+  array.delete_if { |element| element.class != String }
+end
+
+def count_elements(array)
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
+end
+
+def merge_data(keys, values)
+  container = []
+  keys.each do |person_name|
+    name = person_name[:first_name]
+    values.each do |person_data|
+      if person_data[name]
+        merged_person = person_data[name]
+        merged_person[:first_name] = name
+        container << merged_person
+      end
+    end
+  end
+  container
+end
+
+def find_cool(array)
+  cool_hashes = []
+  array.each do |hash|
+    hash.each { |k,v| cool_hashes << hash if k == :temperature && v == "cool"}
+  end
+  cool_hashes
+end
+
+def organize_schools(hash)
+  locations = {}
+  hash.values.each { |location| locations[location.values[0]] = [] }
+  hash.each { |school, location| locations[location.values[0]] << school }
+  locations
+end
